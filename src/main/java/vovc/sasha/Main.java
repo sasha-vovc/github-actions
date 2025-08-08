4b1b837ca36dd5f66fa344e834041f5e42c31dc4
Vovcenco Sasha <vovcenco.sasha.andrei@gmail.com>
2025-08-08 16:07:56 +0300

    Test #1

---
+++ src/main/java/vovc/sasha/Main.java
@@ -0,0 +1,11 @@
+package vovc.sasha;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello world!\n");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
